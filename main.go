1f4395c5ec625454de95d3db45ee91e08812a934
Toma Puljak <toma.puljak@hotmail.com>
2024-02-20 15:40:24 +0000

    fix: add logger

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"os"
+
 	"github.com/daytonaio/daytona/plugins/provisioner"
 	provisioner_manager "github.com/daytonaio/daytona/plugins/provisioner/manager"
+	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-plugin"
 
 	provisioner_plugin "provisioner_plugin/plugin"
 )
 
 func main() {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.Trace,
+		Output:     os.Stderr,
+		JSONFormat: true,
+	})
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: provisioner_manager.ProvisionerHandshakeConfig,
 		Plugins: map[string]plugin.Plugin{
 			"docker-provisioner": &provisioner.ProvisionerPlugin{Impl: &provisioner_plugin.DockerProvisioner{}},
 		},
+		Logger: logger,
 	})
 }
